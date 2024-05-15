773c233e4a46544c3d3a6d6214655376c07cd305
Sandamini Rashmi <127648225+SandaminiRashmi@users.noreply.github.com>
2024-05-15 12:38:31 +0530

    update TypeConverter.kt

--- app/src/main/java/com/coding/meet/todo_app/converters/TypeConverter.kt
+++ app/src/main/java/com/coding/meet/todo_app/converters/TypeConverter.kt
@@ -3,13 +3,16 @@ package com.coding.meet.todo_app.converters
 import androidx.room.TypeConverter
 import java.util.Date
 
+// Custom TypeConverter class for converting Date objects to and from Long timestamps
 class TypeConverter {
 
+    // Convert Long timestamp to Date object
     @TypeConverter
     fun fromTimestamp(value:Long): Date {
         return Date(value)
     }
 
+    // Convert Date object to Long timestamp
     @TypeConverter
     fun dateToTimestamp(date:Date): Long {
         return date.time
