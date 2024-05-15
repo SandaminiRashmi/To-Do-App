c28bf4837198dec43206f61e97d6982b5ba9224c
Sandamini Rashmi <127648225+SandaminiRashmi@users.noreply.github.com>
2024-05-15 12:43:38 +0530

    update TaskDatabase.kt

--- app/src/main/java/com/coding/meet/todo_app/database/TaskDatabase.kt
+++ app/src/main/java/com/coding/meet/todo_app/database/TaskDatabase.kt
@@ -9,25 +9,29 @@ import com.coding.meet.todo_app.converters.TypeConverter
 import com.coding.meet.todo_app.dao.TaskDao
 import com.coding.meet.todo_app.models.Task
 
+// Database class representing the Room database for tasks
 @Database(
     entities = [Task::class],
     version = 1,
     exportSchema = false
 )
-@TypeConverters(TypeConverter::class)
+@TypeConverters(TypeConverter::class)// Use the TypeConverter for Date conversions
 abstract class TaskDatabase : RoomDatabase() {
 
+    // Define the abstract method to access the DAO
     abstract val taskDao : TaskDao
 
     companion object {
         @Volatile
         private var INSTANCE: TaskDatabase? = null
+
+        // Get an instance of the database (singleton pattern)
         fun getInstance(context: Context): TaskDatabase {
             synchronized(this) {
                 return INSTANCE ?: Room.databaseBuilder(
                     context.applicationContext,
                     TaskDatabase::class.java,
-                    "task_db"
+                    "task_db" // Define the name of the database file
                 ).build().also {
                     INSTANCE = it
                 }
